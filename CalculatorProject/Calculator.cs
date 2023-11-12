06fc0588dede4af5570e30c5c8e560052fdc9063
Nikita Kyteyko <nikitakuteyko@gmail.com>
2023-11-12 15:03:36 +0300

    Add project files.

---
+++ CalculatorProject/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProject
+{
+    public class Calculator
+    {
+        public int DivideNumbers(int firstNumber, int secondNumber)
+        {
+            if (secondNumber == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return firstNumber / secondNumber;
+        }
+        public int SumNumbers(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+        public int MultipleNumbers(int firstNumber, int secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+        public int SubstractNumbers(int firstNumber, int secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+    }
+}
---
+++ CalculatorUnitTests/CalculatorTests.cs
@@ -0,0 +1,70 @@
+﻿using CalculatorProject;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CalculatorUnitTests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Div_4Div2_Returned2()
+        {
+            var calc = new Calculator();
+            int firstNumber = 4;
+            int secondNumber = 2;
+            int expectedNumber = 2;
+
+            int result = calc.DivideNumbers(firstNumber, secondNumber);
+            Assert.AreEqual(expectedNumber, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void Div_3Div0_ZeroDivException()
+        {
+            var calc = new Calculator();
+            int firstNumber = 4;
+            int secondNumber = 0;
+
+            int result = calc.DivideNumbers(firstNumber, secondNumber);
+        }
+
+
+        [TestMethod]
+        public void Sum_4Sum2_Returned6()
+        {
+            var calc = new Calculator();
+            int firstNumber = 4;
+            int secondNumber = 2;
+            int expectedNumber = 6;
+
+            int result = calc.SumNumbers(firstNumber, secondNumber);
+            Assert.AreEqual(expectedNumber, result);
+        }
+
+        [TestMethod]
+        public void Mult_4Mult2_Returned8()
+        {
+            var calc = new Calculator();
+            int firstNumber = 4;
+            int secondNumber = 2;
+            int expectedNumber = 8;
+
+            int result = calc.MultipleNumbers(firstNumber, secondNumber);
+            Assert.AreEqual(expectedNumber, result);
+        }
+
+        [TestMethod]
+        public void Sub_6Sub3_Returned3()
+        {
+            var calc = new Calculator();
+            int firstNumber = 6;
+            int secondNumber = 3;
+            int expectedNumber = 3;
+
+            int result = calc.SubstractNumbers(firstNumber, secondNumber);
+            Assert.AreEqual(expectedNumber, result);
+        }
+    }
+}
